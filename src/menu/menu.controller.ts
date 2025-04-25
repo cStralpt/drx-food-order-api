229b65db2f24cbe3ba8a395cac090f88d53cb1fe
satriaali <satrialipatullah@gmail.com>
2025-04-25 22:09:25 +0700

    feat: enhance MenuController with role-based access control and JWT authentication for create, update, and delete operations

--- src/menu/menu.controller.ts
+++ src/menu/menu.controller.ts
@@ -5,16 +5,22 @@ import {
   Get, 
   Param, 
   Patch, 
-  Post 
+  Post,
+  UseGuards 
 } from '@nestjs/common';
 import { MenuService } from './menu.service';
 import { CreateMenuDto, UpdateMenuDto } from './dto';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { RolesGuard } from '../auth/guards/roles.guard';
+import { Roles } from '../auth/decorators/roles.decorator';
 
 @Controller('menus')
 export class MenuController {
   constructor(private readonly menuService: MenuService) {}
 
   @Post()
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('ADMIN')
   create(@Body() createMenuDto: CreateMenuDto) {
     return this.menuService.create(createMenuDto);
   }
@@ -30,16 +36,20 @@ export class MenuController {
   }
 
   @Patch(':id')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('ADMIN')
   update(@Param('id') id: string, @Body() updateMenuDto: UpdateMenuDto) {
     return this.menuService.update(id, updateMenuDto);
   }
 
   @Delete(':id')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('ADMIN')
   delete(@Param('id') id: string) {
-    const  deletedMenu= this.menuService.delete(id);
+    const deletedMenu = this.menuService.delete(id);
     return {
-        message: 'Menu deleted successfully',
-        deletedMenu
+      message: 'Menu deleted successfully',
+      deletedMenu
     }
   }
 }
